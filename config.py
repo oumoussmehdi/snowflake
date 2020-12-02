7bc942f6217128991283e458931375a470cc8e6a
mehdi <mehdi.oumouss@transdev.com>
2020-12-02 14:21:52 +0100

    draft

---
+++ config.py
@@ -0,0 +1,7 @@
+Server = 'put_server_name'
+USER = "put_user_name"
+PASSWORD = "*****"
+ACCOUNT = "account" # this depends on where your snowflake is hosted, for instance aws : ab12345.us-east-1
+WAREHOUSE = "warehouse"
+DATABASE = "db_name"
+SCHEMA = "schema_name"
